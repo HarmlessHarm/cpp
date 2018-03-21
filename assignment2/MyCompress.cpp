3e09312049bdf3412b4f1abed2190af070df8f83
Harm Manders <harmmanders@gmail.com>
2018-03-21 14:52:28 +0100

    parsing args

--- assignment2/MyCompress.cpp
+++ assignment2/MyCompress.cpp
@@ -9,31 +9,71 @@ using namespace  std;
 class MyCompress {
 	
 	string inFile, outFile;
+	char flag;
+	void readFile();
+	void show_help(string);
+
 	public:
-		MyCompress (string, string);
-		void readFile();
+		// MyCompress (string, string);
+		int parseArgs(int, char**);
+		
 
 };
 
-MyCompress::MyCompress (string in, string out) {
-	inFile = in;
-	outFile = out;
-}
+// MyCompress::MyCompress (string in, string out) {
+// 	inFile = in;
+// 	outFile = out;
+// }
 
 void MyCompress::readFile() {
 	// int b = 0;
 }
 
+int MyCompress::parseArgs(int argc, char* argv[]) {
+	// cout << argc << endl;
+	// for (int i = 0; i < argc; i++) {
+	// 	cout << argv[i] << endl;
+	// }
+	if (argc < 2) {
+		show_help(argv[0])
+		return 1;
+	}
+	if (argc == 2) {
+		if ((string) argv[1] == "-h") {
+			show_help(argv[0]);
+			return 1;
+		}
+		else {
+			cout << "Ga iets doen met user input en output" << endl;
+			return 1;
+		}
+	}
+	if (argc == 4) {
+		// Check voor input output files
+	}
+
+}
+
+void MyCompress::show_help(string name) {
+	cerr << "Usage " << name << " [OPTION] [INPUT FILE] [OUTPUT FILE]\n"
+		 << "Options:\n"
+		 << "  -h\tShow this help message\n"
+		 << "  -c\tCompress input to output\n"
+		 << "  -d\tDecompress input to output\n";
+}
 
 int main(int argc, char* argv[]) {
 
-	MyCompress comp ("hoi", "hai");
+	MyCompress comp ();
 
-	// string in = "original.txt";
-	char in[32] = "original.txt";
+	int c = comp.parseArgs(argc, argv);
+
+	if (c == 1) {
+		return 0;
+	}
 	string line;
 	char nextChar;
-	ifstream myfile (in);
+	ifstream myfile (argv[1]);
 	if (myfile.is_open()) {
 		while ( myfile.get(nextChar)  ) {
 			cout << nextChar << '\n';
