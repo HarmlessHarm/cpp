44ebcb36af824440aeae3752764008d469746231
Harm Manders <harmmanders@gmail.com>
2018-03-22 14:10:35 +0100

    Finished restructure

--- assignment2/MyCompress.cpp
+++ assignment2/MyCompress.cpp
@@ -10,47 +10,43 @@ using namespace  std;
 
 class MyCompress {
 	
-	char* inFile;
-	char* outFile;
-	char flag;
-	void show_help(string);
-	int compress();
-	void write(char, int, ostream*&);
+	istream* input_stream = &cin;
+	ostream* output_stream = &cout;
+
+	ifstream ifs;
+	ofstream ofs;
+	
+
+	void write_compress(char, int);
 
 	public:
-		// MyCompress (string, string);
-		int parseArgs(int, char**);
-		
-
+		char flag;
+		MyCompress(int, char*, char*);
+		int compress();
+		static int show_help(string);
 };
 
-// MyCompress::MyCompress (string in, string out) {
-// 	inFile = in;
-// 	outFile = out;
-// }
-
-int MyCompress::compress() {
+MyCompress::MyCompress (int f, char* inFile, char* outFile) {
 	
-	ifstream ifs;
-	istream* input_stream = &cin;
-
-	ofstream ofs;
-	ostream* output_stream = &cout;
-	
+	flag = f;
+		
 	if (strlen(inFile) != 0) {
 		ifs.open(inFile);
 		input_stream = &ifs;
 	}
+	else cerr << "Use ^D when finished to declare EOF" << endl; 
 	if (strlen(outFile) != 0) {
-		cout << outFile << " used!" << endl;
 		ofs.open(outFile);
 		output_stream = &ofs;
 	}
+}
 
+int MyCompress::compress() {
+	
 	string line;
 	char next;
-	int pvalid = 0;
 	char prev;
+	int pvalid = 0;
 	int n = 1;
 
 	while ( input_stream->get(next)  ) {
@@ -63,17 +59,18 @@ int MyCompress::compress() {
 			n++;
 		}
 		else {
-			write(prev, n, output_stream);
+			write_compress(prev, n);
 			prev = next;
 			n = 1;
 		}
 	}
-	write(prev, n, output_stream);
+	// Write last read char.
+	write_compress(prev, n);
 
 	return 0;
 }
 
-void MyCompress::write(char prev, int n, ostream*& output_stream) {
+void MyCompress::write_compress(char prev, int n) {
 	
 	vector<char> buff;
 	if (isdigit(prev) || prev == '\\') {
@@ -81,75 +78,100 @@ void MyCompress::write(char prev, int n, ostream*& output_stream) {
 	}
 	buff.push_back(prev);
 	if (n > 1) {
-		// To cast int to char  (char)n didnt work as it converts to ascii
+		// To cast int to char. (char)n didnt work as it converts to ascii
 		string s = to_string(n);
-		cout << s << endl;
 		char* c = &s[0];
 		for (int i = 0; i < strlen(c); i++) {
 			buff.push_back(c[i]);
 		}
-		// char c = s[0];
-		// buff.push_back(c);
 	}
-	// for ()
 	for (auto i = buff.begin(); i != buff.end(); i++) {
-		// cout << *i;
 		*output_stream << *i;
 	}
 }
 
-int MyCompress::parseArgs(int argc, char* argv[]) {
-
-	if (argc < 2) {
-		show_help(argv[0]);
-		return 1;
-	}
-	if (argc == 2) {
-		if ((string) argv[1] == "-h") {
-			show_help(argv[0]);
-			return 1;
-		}
-		else {
-			cout << "Ga iets doen met user input en output" << endl;
-			return 1;
-		}
-	}
-	if (argc == 3) {
-		if ( (string) argv[1] == "-c" ) {
-			inFile = argv[2];
-			outFile = (char*) "";
-			// cout << outFile << endl;
-			return compress();
-		}
-	}
-	if (argc == 4) {
-		// TODO: Check voor input output files
-		if ( (string) argv[1] == "-c" ) {
-			inFile = argv[2];
-			outFile = argv[3];
-			return compress();
-		}
-	}
-	return 0;
-
-}
-
-void MyCompress::show_help(string name) {
-	cerr << "Usage " << name << " [OPTION] [INPUT FILE] [OUTPUT FILE]\n"
+int MyCompress::show_help(string name) {
+	cerr << "Usage: " << name << " <OPTION> [INPUT FILE] [OUTPUT FILE]\n"
 		 << "Options:\n"
 		 << "  -h\tShow this help message\n"
 		 << "  -c\tCompress input to output\n"
-		 << "  -d\tDecompress input to output\n";
+		 << "  -d\tDecompress input to output\n"
+		 << '\n'
+		 << "  -o\tSpecify output file\n"
+		 << "  -i\tSpecify input file\n"
+		 << "If no output file is provided output is sent to terminal\n"
+		 << "If no input file is provided input is provided by terminal\n";
+}
+
+bool exists (string name) {
+    ifstream f(name.c_str());
+    return f.good();
 }
 
 int main(int argc, char* argv[]) {
 
-	MyCompress comp;
+	char* inFile = (char*) "";
+	char* outFile = (char*) "";
+	char flag = 'c';
+	char flagSet = 0;
 
-	int c = comp.parseArgs(argc, argv);
-
-	if (c == 1) {
-		return 0;
+	if (argc < 2) {
+		return MyCompress::show_help(argv[0]);
 	}
+	if (argc >= 2) {
+		for (int i = 1; i < argc; i++) {
+			string arg = argv[i];
+			if (arg == "-h") {
+				return MyCompress::show_help(argv[0]);
+			}
+			else if (arg == "-c" && !flagSet) {
+				flag = 'c';
+				flagSet = 1;
+			}
+			else if (arg == "-d" && !flagSet) {
+				flag = 'd';
+				flagSet = 1;
+			}
+			else if (arg == "-o") {
+				if (i + 1 < argc) {
+					outFile = argv[i + 1];
+					i++;
+				}
+				else {
+					cerr << "-o requires an argument" << endl;
+					return 1;
+				}
+			}
+			else if (arg == "-i") {
+				if (i + 1 < argc) {
+					inFile = argv[i + 1];
+					i++;
+				}
+				else {
+					cerr << "-i requires an argument" << endl;
+					return 1;
+				}
+			}
+			else if (strlen(inFile) == 0) {
+				if (exists(arg)) {
+					inFile = argv[i];
+				}
+				else {MyCompress
+					cerr << arg << " doesn't exist!" << endl;
+					return 1;
+				}
+			}
+			else if (strlen(outFile) == 0) {
+				outFile = argv[i];
+			}
+		}
+	}
+	
+	cout << flag << " i: " << inFile << " o: " << outFile << endl;
+	MyCompress compressor (flag, inFile, outFile);
+	if (compressor.flag == 'c') {
+		return compressor.compress();
+	}
+	// else return compressor.compress();
 	return 0;
 }
